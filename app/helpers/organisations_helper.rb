1775aa09f63a9fa0fe3055138f472e697e1f515d
Lucia Harcegova <fakeluciamail@gmail.com>
2022-03-11 12:17:07 +0100

    adjusted new org tests

--- app/helpers/organisations_helper.rb
+++ app/helpers/organisations_helper.rb
@@ -1,2 +1,25 @@
+require "json"
+require "open-uri"
+
 module OrganisationsHelper
+
+  class Address
+    def initialize(street,zip_codes)
+      @street = street
+      @city = ""
+      @zip_code = zip_code
+      @country = Denmark
+    end
+
+    def format_address
+      url = "https://parseapi.back4app.com/classes/DK_DK?limit=10&keys=place,postalCode"
+      user_serialized = URI.open(url).read
+      user = JSON.parse(user_serialized)
+
+
+      return "#{@street},#{@city},#{@zip_code},#{country}"
+    end
+
+  end
+
 end
--- spec/models/organisation_spec.rb
+++ spec/models/organisation_spec.rb
@@ -5,9 +5,11 @@ RSpec.describe Organisation do
   context 'newly created' do
     let!(:category){Category.create(name: "Environment")}
     let!(:organisation){FactoryBot.create(:organisation)}
+    let!(:search_word){SearchWord.create(search_word: "sustainability")}
 
     before do
       OrganisationCategory.create(category: category, organisation: organisation)
+      Tag.create(search_word: search_word, organisation: organisation)
     end
 
     it 'has a name' do
@@ -31,17 +33,17 @@ RSpec.describe Organisation do
 
 
     it 'belongs to at least one existing category' do
-      puts Category.all
-      puts OrganisationCategory.all
       org_category = organisation.category_ids
-      expect(org_category).not_to be_nil
+      expect(org_category).not_to be_empty
     end
 
     it 'has associated tags' do
-      org2 = build(:organisation, name: "Unique name", tags: [])
-      expect(organisation.search_words).to exist
-      expect(org2).not_to be_valid
+      org_tags = organisation.tags
+      org_search_words = organisation.search_word_ids
+      expect(org_tags).not_to be_nil
+      expect(org_search_words).not_to be_empty
     end
+
     it 'has a valid address' do
       copenhagen_zip_codes = ['2000', '2100', '2150', '2200', '2300', '2400','2450']
       city = ["Copenhagen", "København", 'Hvidorve', 'Glostrup', 'Hellerup']
