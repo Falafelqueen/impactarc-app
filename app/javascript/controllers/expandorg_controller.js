e4728f8b16eb31a5c9b7542913d94cede7a5f7a1
Lucia Harcegova <fakeluciamail@gmail.com>
2022-04-28 17:24:06 +0200

    mobile responsive css grid

--- app/javascript/controllers/expandorg_controller.js
+++ app/javascript/controllers/expandorg_controller.js
@@ -2,23 +2,46 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller{
   static targets = ["expandorgbtn", "closepreviewbtn", "orgpreview"]
+  something = window.scrollY
 
   showOrganisation(e){
     const unsaveOrgBtn = document.querySelector(".save-org-btn-icon")
     const contentDiv = document.querySelector(".content-container")
     const contentDivPosition = contentDiv.getBoundingClientRect()
+    const orgCards = document.querySelectorAll(".org-card")
+    const body = document.querySelector("body")
+
     const top = 200
     console.log(screen.height);
     console.log(contentDiv, contentDivPosition, top);
     console.log(window.scrollY, window.pageYOffset);
-    if(window.scrollY >= 200){
+
+    console.log(this.something);
+/*     body.classList.add("stop-scrolling") */
+
+   /*  if(window.scrollY >= 200){
       this.orgpreviewTarget.style.top = `${window.scrollY - top}px`
-    }
-    if(!e.target.matches(".dont-open-preview-btn")){
+    } */
+    const arrayWindowHeights = []
+    if(!e.target.matches(".dont-open-preview-btn") && !e.target.matches(".close-btn-icon")){
+      const originalWindowScroll = window.pageYOffset
+      arrayWindowHeights.push(originalWindowScroll)
+      console.log("originalwindowscroll:", originalWindowScroll, "array:", arrayWindowHeights);
       this.orgpreviewTarget.hidden = false
+      window.scrollTo({
+        top: 200,
+        left: 500
+      })
+      arrayWindowHeights
     }
-  }
-  showIndex(e){
-    this.orgpreviewTarget.hidden = true
+
+    if(e.target.matches(".close-btn-icon")){
+       this.orgpreviewTarget.hidden = true
+       console.log(arrayWindowHeights);
+      /*  window.scrollTo({
+         top: originalWindowScroll,
+         left: 500
+       }) */
+     }
   }
 }
